0144dd265ac2df6b973de64251f81bd22ffbcf42
niabicht <nj_abicht@outlook.com>
2025-09-18 19:15:41 +0200

    some renaming of constants

--- Runtime/Constants.cs
+++ Runtime/Constants.cs
@@ -6,8 +6,8 @@ namespace DeadWrongGames.ZTools
         public const string ROOT_FOLDER_NAME = "Assets";
         public const string PROJECT_FOLDER_NAME = "_Project";
         public const string SERVICES_FOLDER_NAME = "Services";
-        public const string SERVICES_ASSETS_NAME = "Assets";
-        public const string SERVICES_ASSETS_EVENT_CHANNEL_SO_NAME = "EventChannelSO";
-        public const string SERVICES_ASSETS_SOUND_DATA_SO_NAME = "SoundDataSO";
+        public const string SERVICES_ASSETS_FOLDER_NAME = "Assets";
+        public const string SERVICES_EVENT_CHANNEL_SO_FOLDER_NAME = "EventChannelSO";
+        public const string SERVICES_SOUND_DATA_SO_FOLDER_NAME = "SoundDataSO";
     }
 }
